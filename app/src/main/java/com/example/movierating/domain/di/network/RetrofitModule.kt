a06f3ba4ac6c5fc35d178c4136fa3e011a96bc71
vladuslavbakumenko <vladgts32@gmail.com>
2025-11-17 22:09:48 +0000

    .

--- app/src/main/java/com/example/movierating/domain/di/network/RetrofitModule.kt
+++ app/src/main/java/com/example/movierating/domain/di/network/RetrofitModule.kt
@@ -11,7 +11,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 class RetrofitModule {
